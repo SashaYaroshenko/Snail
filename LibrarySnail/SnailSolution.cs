ae02c381fc9a75b349071ea410bc3bbffdb301d7
Denys <navrotskyidenys@gmail.com>
2023-03-30 22:06:48 +0300

    Working with one bug (you mast find it), need todo NUnit

--- LibrarySnail/SnailSolution.cs
+++ LibrarySnail/SnailSolution.cs
@@ -2,67 +2,77 @@
 {
     public class SnailSolution
     {
-            public static int[] Snail(int[,] array)
-            {
+        public static int[] Snail(int[,] array)
+        {
             List<int> result = new List<int>();
-            int zero = 0;
-            int one = 1;
-            //int col = 0;
-            //int row = 0;
-            for (int row = 0; row < array.GetLength(0) / 2; row++)
-            {
-                for (int col = 0; col < array.GetLength(1) / 2; col++)
-                {
-                    if (array[array.GetLength(0) / 2, array.GetLength(1) / 2] == array[row, col])
-                    {
-                        if (array.GetLength(0) % 2 != 0)
-                        {
-                            result.Add(array[row, col]);
-                        }
-                        break;
-                    }
-                    while (col < array.GetLength(1) - zero)  
-                    {
-                        if (col == array.GetLength(1) - one) 
-                        {
-                            result.Add(array[row, col]);
-                            while (row < array.GetLength(0) - zero)  
-                            {
-                                if (row == array.GetLength(0) - one)
-                                {
-                                    while (col != zero)
-                                    {
-                                        col--;
-                                        result.Add(array[row, col]);
-                                    }
-                                    while (row != one)
-                                    {
-                                        row--;
-                                        result.Add(array[row, col]);
-                                    }
+            int dx = 1;
+            int dy = 1;
 
+            int row = 0;
+            int col = 0;
+
+            while (col <= array.GetLength(1) - dx)
+            {
+                if (col == array.GetLength(1) - dx)
+                {
+                    while (row <= array.GetLength(0) - dy)
+                    {
+                        if (row == array.GetLength(0) - dy)
+                        {
+                            while (col >= dx-1)
+                            {
+                                if (col == dx-1)
+                                {
+                                    dx++;
+                                }
+                                else
+                                {
+                                    result.Add(array[row, col]);
+                                    col--;
+                                }
+                                
+                            }
+
+                            while (row >= dy-1)
+                            {
+                                if (row == dy-1)
+                                {
+                                    dy++;
+                                    row++;
+                                    col++;
                                     break;
                                 }
                                 else
                                 {
-                                    row++;
                                     result.Add(array[row, col]);
-                                }
+                                    row--;
+                                }                             
                             }
                             break;
                         }
                         else
                         {
                             result.Add(array[row, col]);
-                            col++;
+                            row++;
                         }
-
                     }
-                    zero++;
-                    one++;
-
                 }
-                break;
+                else
+                {
+                    result.Add(array[row, col]);
+                    col++;
+                }
+                if (row > array.GetLength(0) - dy) break;
+            }
+            row--;
+            col--;
+
+            if (array[array.GetLength(0) / 2, array.GetLength(1) / 2] == array[row, col])
+            {
+                if (array.GetLength(0) % 2 != 0)
+                {
+                    result.Add(array[row, col]);
+                }
             }
             return result.ToArray();
         }
