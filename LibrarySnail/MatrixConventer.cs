12d5c49d3f4bbe9f59e91f41a857d93b9a958d12
Denys <navrotskyidenys@gmail.com>
2023-04-14 01:18:16 +0300

    Change algorithm for snail

---
+++ LibrarySnail/MatrixConventer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibrarySnail
+{
+    internal class MatrixConventer
+    {
+        public static int[][] ConvertMatrix(int[,] matrix)
+        {
+            int lenRows = matrix.GetLength(0);
+            int[][] result = new int[lenRows][];
+            int lenCols = matrix.GetLength(1);
+            for(int i = 0; i < lenRows; i++)
+            {
+                result[i] = new int[lenCols]; 
+                for(int j = 0; j < lenCols; j++)
+                {
+                    result[i][j]=matrix[i,j];
+                }
+            }
+            return result;
+        }
+    }
+}
--- LibrarySnail/SnailSolution.cs
+++ LibrarySnail/SnailSolution.cs
@@ -2,7 +2,7 @@
 {
     public class SnailSolution
     {
-        public static int[] Snail(int[,] array)
+        public static int[] Snail(int[][] array)
         {
             List<int> result = new List<int>();
             int dx = 1;
@@ -11,70 +11,47 @@
             int row = 0;
             int col = 0;
 
-            while (col <= array.GetLength(1) - dx)
+            int len = array[row].GetLength(0) * array.GetLength(0);
+
+            while (dx + dx <= array[row].GetLength(0) || dy + dy <= array.GetLength(0))
             {
-                if (col == array.GetLength(1) - dx)
+                while (col <= array[row].GetLength(0) - dx)
                 {
-                    while (row <= array.GetLength(0) - dy)
-                    {
-                        if (row == array.GetLength(0) - dy)
-                        {
-                            while (col >= dx-1)
-                            {
-                                if (col == dx-1)
-                                {
-                                    dx++;
-                                }
-                                else
-                                {
-                                    result.Add(array[row, col]);
-                                    col--;
-                                }
-                                
-                            }
-
-                            while (row >= dy-1)
-                            {
-                                if (row == dy-1)
-                                {
-                                    dy++;
-                                    row++;
-                                    col++;
-                                    break;
-                                }
-                                else
-                                {
-                                    result.Add(array[row, col]);
-                                    row--;
-                                }                             
-                            }
-                            break;
-                        }
-                        else
-                        {
-                            result.Add(array[row, col]);
-                            row++;
-                        }
-                    }
-                }
-                else
-                {
-                    result.Add(array[row, col]);
+                    result.Add(array[row][col]);
                     col++;
-                }
-                if (row > array.GetLength(0) - dy) break;
+                }   col--; row++;
+                if (result.Count == len) break;
+                while (row <= array.GetLength(0) - dy)
+                {
+                    result.Add(array[row][col]);
+                    row++;
+                }   row--; col--;
+                if (result.Count == len) break;
+                while (col >= dx - 1)
+                {
+                    result.Add(array[row][col]);
+                    col--;
+                }   col++; row--;
+                if (result.Count == len) break;
+                while (row > dy - 1)
+                {
+                    result.Add(array[row][col]);
+                    row--;
+                }   row++; col++;
+                if (result.Count == len) break;            
+                dx++;
+                dy++;
             }
-            row--;
-            col--;
 
-            if (array[array.GetLength(0) / 2, array.GetLength(1) / 2] == array[row, col])
+            if (array.GetLength(0) == array[row].GetLength(0))
             {
                 if (array.GetLength(0) % 2 != 0)
                 {
-                    result.Add(array[row, col]);
+                    result.Add(array[row][col]);
                 }
             }
             return result.ToArray();
         }
+
     }
 }
